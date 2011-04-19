4149bc479b81a7af443ae872a64ce556c120fe58
Michal Okoniewski <michal@fgcz.ethz.ch>
2011-04-19 19:21:00 +0000

    Sample commit
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_8/madman/Rpacks/rnaSeqMap@54943 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/bio_sample.sql
+++
@@ -1,1 +0,0 @@
-insert into bio_sample values ((1,B),(2,T),(3,B),(4,B),(5,T),(6,T));
--- inst/scripts/indices.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `xmapcore_homo_sapiens_58`.`seq_read` 
-ADD INDEX `common` (`sample_id` ASC, `chromosome_id` ASC, `start` ASC, `rend` ASC, `strand` ASC) ;
-
