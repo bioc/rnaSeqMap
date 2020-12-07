83b5b4c5e5963c9cad078de857f81cfe3489433f
Lori Shepherd <lori.shepherd@roswellpark.org>
2020-12-07 13:11:29 -0500

    Deprecate

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,6 @@
-.onLoad = function(lib,pkg) {
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.14")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
-
